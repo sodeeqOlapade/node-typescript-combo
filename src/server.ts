2dbaec93ee03dc9bfd6b6009da6868ad5cab1891
Sodeeq Olapade <solapade@seamfix.com>
2019-10-24 14:36:35 +0100

    implement routes structure

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import http from "http";
 import express from "express";
-import { applyMiddleware } from "./utils";
+import { applyMiddleware, applyRoutes } from "./utils";
 import middleware from "./middleware";
+import routes from "./services/search";
 
 const app = express();
 applyMiddleware(middleware, app);
+applyRoutes(routes, app);
 
 const { PORT = 3000 } = process.env;
 const server = http.createServer(app);
