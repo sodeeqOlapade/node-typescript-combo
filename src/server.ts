6b4b6dafdbddee6a0f0339f64debd8a7f4e18ad0
Sodeeq Olapade <solapade@seamfix.com>
2019-10-24 15:33:48 +0100

    setuo error handlers

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,23 @@ import express from "express";
 import { applyMiddleware, applyRoutes } from "./utils";
 import middleware from "./middleware";
 import routes from "./routes";
+import errorHandlers from "./middleware/errorHandlers"
+
+
+process.on("uncaughtException", e => {
+  console.log(e);
+  process.exit(1);
+});
+process.on("unhandledRejection", e => {
+  console.log(e);
+  process.exit(1);
+});
+
 
 const app = express();
 applyMiddleware(middleware, app);
 applyRoutes(routes, app);
+applyMiddleware(errorHandlers, app)
 
 const { PORT = 3000 } = process.env;
 const server = http.createServer(app);
